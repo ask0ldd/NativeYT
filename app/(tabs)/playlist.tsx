979a42c6e485689c93f161b84d4bb37df6678a19
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-27 16:28:52 +0200

    *add* front / playlist page including related items

--- app/(tabs)/playlist.tsx
+++ app/(tabs)/playlist.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 
+import PlaylistVideoItem from '@/components/PlaylistVideoItem';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -7,10 +8,32 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function TabThreeScreen() {
   return (
     <ThemedView style={styles.container}>
-        <SafeAreaView style={styles.bgred}>
-            <ThemedText style={styles.title}>AAAAAAAAAAA</ThemedText>
-            <ThemedText>AAAAAAAAAAA</ThemedText>
-        </SafeAreaView>
+      <SafeAreaView style={styles.safe}>
+        <ThemedText style={styles.title}>Playlist</ThemedText>
+        <ScrollView
+          contentContainerStyle={styles.scrollContent}
+          showsVerticalScrollIndicator={false}
+        >
+          <Text style={{color:'#fff', width:'100%'}}>aaaaaa</Text>
+          <Text style={{color:'#fff', width:'100%'}}>aaaaaa</Text>
+          <PlaylistVideoItem videoId={'k0VOchBlqiQ'}/>
+          <PlaylistVideoItem videoId={'45zq0Xct-Oc'}/>
+          <PlaylistVideoItem videoId={'DZWNMjhj-VY'}/>
+          <PlaylistVideoItem videoId={'VrSJhTGMM90'}/>
+          <PlaylistVideoItem videoId={'k0VOchBlqiQ'}/>
+          <PlaylistVideoItem videoId={'45zq0Xct-Oc'}/>
+          <PlaylistVideoItem videoId={'DZWNMjhj-VY'}/>
+          <PlaylistVideoItem videoId={'VrSJhTGMM90'}/>
+          <PlaylistVideoItem videoId={'k0VOchBlqiQ'}/>
+          <PlaylistVideoItem videoId={'45zq0Xct-Oc'}/>
+          <PlaylistVideoItem videoId={'DZWNMjhj-VY'}/>
+          <PlaylistVideoItem videoId={'VrSJhTGMM90'}/>
+          <PlaylistVideoItem videoId={'k0VOchBlqiQ'}/>
+          <PlaylistVideoItem videoId={'45zq0Xct-Oc'}/>
+          <PlaylistVideoItem videoId={'DZWNMjhj-VY'}/>
+          <PlaylistVideoItem videoId={'VrSJhTGMM90'}/>
+        </ScrollView>
+      </SafeAreaView>
     </ThemedView>
   );
 }
@@ -24,15 +47,20 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 20,
+    textAlign: 'center',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  bgred:{
-    backgroundColor:'#ff0000',
+  safe: {
     flex: 1,
-    width:'100%'
-  }
+    width: '100%',
+  },
+  scrollContent: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    columnGap: 10,
+    rowGap: 10,
+    paddingHorizontal: 10,
+    paddingBottom: 20,
+  },
 });
