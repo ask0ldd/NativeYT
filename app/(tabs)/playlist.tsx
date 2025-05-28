b73b56dc19d3069748f0db92ab72b0de264fd6ea
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-28 18:25:03 +0200

    *edit* front / playlist page styling

--- app/(tabs)/playlist.tsx
+++ app/(tabs)/playlist.tsx
@@ -1,37 +1,34 @@
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput } from 'react-native';
 
 import PlaylistVideoItem from '@/components/PlaylistVideoItem';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import mockVideosService from '@/services/mockVideosService';
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function TabThreeScreen() {
+
+  const [searchInputValue, setSearchInputValue]= useState<string>("Search for a Video")
+
+  function shorten(text : string, length : number) : string{
+    if(text.length > length) return text.slice(0, length) + '...'
+    return text
+  }
+
   return (
     <ThemedView style={styles.container}>
       <SafeAreaView style={styles.safe}>
         <ThemedText style={styles.title}>Playlist</ThemedText>
+        <TextInput style={{fontSize:14, color:'#93AEB5', borderRadius:4, fontFamily:'Montserrat_500Medium', backgroundColor:'#55373722', marginHorizontal:10, paddingHorizontal:10}} value={searchInputValue} onChangeText={setSearchInputValue} onFocus={() => {if(searchInputValue == "Search for a Video") setSearchInputValue("")}} onBlur={() => {if(searchInputValue == "") setSearchInputValue("Search for a Video")}}/>
         <ScrollView
           contentContainerStyle={styles.scrollContent}
           showsVerticalScrollIndicator={false}
         >
-          <Text style={{color:'#fff', width:'100%'}}>aaaaaa</Text>
-          <Text style={{color:'#fff', width:'100%'}}>aaaaaa</Text>
-          <PlaylistVideoItem videoId={'k0VOchBlqiQ'}/>
-          <PlaylistVideoItem videoId={'45zq0Xct-Oc'}/>
-          <PlaylistVideoItem videoId={'DZWNMjhj-VY'}/>
-          <PlaylistVideoItem videoId={'VrSJhTGMM90'}/>
-          <PlaylistVideoItem videoId={'k0VOchBlqiQ'}/>
-          <PlaylistVideoItem videoId={'45zq0Xct-Oc'}/>
-          <PlaylistVideoItem videoId={'DZWNMjhj-VY'}/>
-          <PlaylistVideoItem videoId={'VrSJhTGMM90'}/>
-          <PlaylistVideoItem videoId={'k0VOchBlqiQ'}/>
-          <PlaylistVideoItem videoId={'45zq0Xct-Oc'}/>
-          <PlaylistVideoItem videoId={'DZWNMjhj-VY'}/>
-          <PlaylistVideoItem videoId={'VrSJhTGMM90'}/>
-          <PlaylistVideoItem videoId={'k0VOchBlqiQ'}/>
-          <PlaylistVideoItem videoId={'45zq0Xct-Oc'}/>
-          <PlaylistVideoItem videoId={'DZWNMjhj-VY'}/>
-          <PlaylistVideoItem videoId={'VrSJhTGMM90'}/>
+          <Text style={{color:'#fff', width:'100%', marginTop:20}}>Results :</Text>
+          {mockVideosService.getAll().map(
+            video => <PlaylistVideoItem key={video.YoutubeId} title={shorten(video.title, 50)} videoId={video.YoutubeId} duration={video.duration}/>
+          )}
         </ScrollView>
       </SafeAreaView>
     </ThemedView>
