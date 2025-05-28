77ffd6d3365e72b47390f30234deb7629abec11c
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-28 20:49:55 +0200

    *edit* front / playlist item style revamped

--- components/PlaylistVideoItem.tsx
+++ components/PlaylistVideoItem.tsx
@@ -8,15 +8,17 @@ export default function PlaylistVideoItem({ videoId, title, duration } : { video
     }
 
     return(
-        <View style={{flexGrow:1, flexShrink:1, width:'40%', padding:10, backgroundColor:'#55373722', borderRadius:4, boxShadow:'0 2px 12px 0 #00000088' }}>
-            <Link href={{ pathname: "/playvideo", params: { videoId } }} style={{ width:'100%', flexDirection:'column', rowGap:10}} asChild>
+        <View style={{width:'100%', height:100, padding:10, backgroundColor:'#55373722', borderRadius:4, boxShadow:'0 2px 12px 0 #00000088' }}>
+            <Link href={{ pathname: "/playvideo", params: { videoId } }} style={{ width:'100%', flexDirection:'row', columnGap:10 }} asChild>
                 <Pressable>
-                    <Text style={{color:'#93AEB5', fontSize:12, lineHeight:20}}>{title}</Text>
-                    <Image
-                        source={{ uri: `https://img.youtube.com/vi/${videoId}/default.jpg` }}
-                        style={{ width:'100%', height:undefined, aspectRatio:120/90, borderRadius:4 }}
-                    />
-                    <Text style={{color:'#93AEB5', fontSize:10, lineHeight:20}}>{parseISODuration(duration)}</Text>
+                    <View style={{width:'35%', height:80}}>
+                        <Image
+                            source={{ uri: `https://img.youtube.com/vi/${videoId}/default.jpg` }}
+                            style={{ flex: 1, borderRadius:4 }}
+                        />
+                        <Text style={{position :'absolute', bottom : 4, right : 4, backgroundColor: '#00000099', borderRadius:4, paddingHorizontal:8, paddingVertical : 2, color : '#93AEB5', fontSize:10, lineHeight:20}}>{parseISODuration(duration)}</Text>
+                    </View>
+                    <Text style={{color:'#93AEB5', fontSize:12, lineHeight:20, flex : 1, flexWrap: 'wrap'}}>{title}</Text>
                 </Pressable>
             </Link>
         </View>
