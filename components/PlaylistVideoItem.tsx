195ec440f22f9ccde360e81f701bff5400094675
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-05-30 03:21:21 +0200

    *edit* front / playlist video item styling

--- components/PlaylistVideoItem.tsx
+++ components/PlaylistVideoItem.tsx
@@ -1,11 +1,8 @@
+import TimeService from "@/services/timeService";
 import { Link } from "expo-router";
 import { Image, Pressable, Text, View } from "react-native";
 
 export default function PlaylistVideoItem({ videoId, title, duration } : { videoId : string, title : string, duration : string }){
-
-    function parseISODuration(isoDuration: string): string {
-        return isoDuration.replace('PT', '').replace("M", ":").replace("S", "")
-    }
 
     return(
         <View style={{width:'100%', height:100, padding:10, backgroundColor:'#55373722', borderRadius:4, boxShadow:'0 2px 12px 0 #00000088' }}>
@@ -16,7 +13,9 @@ export default function PlaylistVideoItem({ videoId, title, duration } : { video
                             source={{ uri: `https://img.youtube.com/vi/${videoId}/default.jpg` }}
                             style={{ flex: 1, borderRadius:4 }}
                         />
-                        <Text style={{position :'absolute', bottom : 4, right : 4, backgroundColor: '#00000066', borderRadius:4, paddingHorizontal:8, paddingVertical : 2, color : '#93AEB5', fontSize:10, lineHeight:20}}>{parseISODuration(duration)}</Text>
+                        <Text style={{position :'absolute', bottom : 4, right : 4, backgroundColor: '#00000066', borderRadius:4, paddingHorizontal:8, paddingVertical : 2, color : '#93AEB5', fontSize:10, lineHeight:20}}>
+                            {TimeService.parseISODuration(duration)}
+                        </Text>
                     </View>
                     <View style={{flexDirection:'column', width:'65%', flexShrink:1}}>
                         <Text style={{color:'#FFFFFFcc', fontSize:14, lineHeight:20, flexWrap: 'wrap'}}>{title}</Text>
